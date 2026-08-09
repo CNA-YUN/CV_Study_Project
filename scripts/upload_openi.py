3dce6bd4653dc2def2f04bb9ed58b5f9e1438cf5
CNA-YUN <zhangyun20210801@163.com>
2026-08-09 10:19:18 +0800

    stop track scripts/upload_openi.py

--- scripts/upload_openi.py
+++
@@ -1,9 +0,0 @@
-import openi
-from openi import openi_upload_file
-from _init_ import DATA_DIR
-
-openi.login(token='0ec626ba6fdac57e828ec0b3bb67601b544b2483')
-
-openi_upload_file(repo_id="zhangyun_cqupt/OxfordPet",
-                  file_or_folder_path=DATA_DIR / 'OxfordPet',
-                  max_workers=50)

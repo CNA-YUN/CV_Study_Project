5b17df7589bf978b46b09a7a370ca3905c38f76e
CNA-YUN <zhangyun20210801@163.com>
2026-08-09 19:38:40 +0800

    remove download_from_OpenI.py from remote repo

--- scripts/download_from_openi.py
+++
@@ -1,7 +0,0 @@
-# 从 OpenI 下载数据集或模型
-import openi
-from openi import openi_download_file
-from _init_ import DATA_DIR
-openi.login(token='0ec626ba6fdac57e828ec0b3bb67601b544b2483')
-
-openi_download_file("ppsuser/msd_Task09_Spleen", repo_type="dataset", local_dir=DATA_DIR+"/msd_Task09_Spleen", max_workers=10)

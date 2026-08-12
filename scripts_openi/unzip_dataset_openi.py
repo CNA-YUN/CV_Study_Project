1db4188763a38cfa55aa4bf52af31be92a407bcd
CNA-YUN <zhangyun20210801@163.com>
2026-08-12 11:49:18 +0800

    unzip dataset on OpenI

---
+++ scripts_openi/unzip_dataset_openi.py
@@ -0,0 +1,19 @@
+import zipfile
+import os
+from tqdm import tqdm  # 导入进度条库
+from c2net.context import prepare
+
+c2net_context = prepare()
+
+source_zip = c2net_context.dataset_path + "/my_file.zip"
+target_dir = c2net_context.output_path + "/Output_Folder"
+os.makedirs(target_dir, exist_ok=True)
+
+with zipfile.ZipFile(source_zip, 'r') as zip_ref:
+    file_list = zip_ref.infolist()
+
+    # tqdm 自动显示进度百分比、速度和剩余时间
+    for file_info in tqdm(file_list, desc="解压中", unit="个文件"):
+        zip_ref.extract(file_info, path=target_dir)
+
+print("全部解压完成！")
